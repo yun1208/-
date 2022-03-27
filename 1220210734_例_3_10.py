0ae652aa0c6b3f9d85601809c61b84de1b1776bf
yun1208 <102461425+yun1208@users.noreply.github.com>
2022-03-27 17:20:31 +0800

    Update 1220210734_例_3_10.py

--- 1220210734_例_3_10.py
+++ 1220210734_例_3_10.py
@@ -5,6 +5,6 @@ for i in range(games):
     result=random.randint(0,1)
     if result==1:#获胜
         print("朋友圈炫耀战绩")
-    else: result==0失败
+    else:#result==0失败
         print("沉默...")
     print("*"*20)
