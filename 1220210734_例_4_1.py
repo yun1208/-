38837d27bb0a28af4617f6e8d8b368c2611b08fc
yun1208 <102461425+yun1208@users.noreply.github.com>
2022-03-27 19:02:43 +0800

    Create 1220210734_例_4_1.py

---
+++ 1220210734_例_4_1.py
@@ -0,0 +1,12 @@
+aaa="hello world"
+print(aaa[0])
+print(aaa[-1])
+print(aaa[2])
+bb=("宝马 ","奥迪","奔驰","路虎")
+print(bbb[0])
+print(bbb[-1])
+print(bbb[2])
+cce=["宝马 ","奥迪","奔驰","路虎"]
+print(ccc[0])
+print(ccc[-1])
+print(ccc[2])
